5323de00eda7586b149a3aebf52453686e4f6ecb
Tulsi Shah <tulsishah@google.com>
2026-01-28 05:20:29 +0000

    test changes

---
+++ math/math.go
@@ -0,0 +1,15 @@
+package math
+
+// Add takes two integers and returns their sum
+func Add(a, b int) int {
+    return a + b
+}
+
+func Absolute(n int) int {
+    if n < 0 {
+        // This is the "negative path"
+        return -n
+    }
+    // This is the "positive path"
+    return n
+}
